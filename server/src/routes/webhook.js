56b1990deb425b7065fa8be1a13d6579f0bc2278
Pravesh <praveshchettri05@gmail.com>
2026-08-29 21:18:06 +0530

    Fix livekit import in webhook route

--- server/src/routes/webhook.js
+++ server/src/routes/webhook.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { WebhookReceiver } = require('@livekit/livekit-server-sdk');
+const { WebhookReceiver } = require('livekit-server-sdk');
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
