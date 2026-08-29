c772642d36a35a9b5831ac2454c38af69bdecf94
Pravesh <praveshchettri05@gmail.com>
2026-08-29 21:07:54 +0530

    Fix livekit-server-sdk package name

--- server/src/routes/livekit.js
+++ server/src/routes/livekit.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { AccessToken } = require('@livekit/livekit-server-sdk');
+const { AccessToken } = require('livekit-server-sdk');
 const { PrismaClient } = require('@prisma/client');
 const { verifyFirebaseToken } = require('../middleware/auth');
 
